4c9d1c69ba825296cef26945b9c44cca2f3cd530
Thomas Buida <thomasbuida@gmail.com>
2016-02-03 14:23:42 -0600

    Finished product display for seasonal-sales

--- spa/chaining/chaining.js
+++ spa/chaining/chaining.js
@@ -7,6 +7,6 @@
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-integers = integers.sort(function (a, b) { return b - a}).filter(function(i) {return i > 19}).map(function(i) { return (i * 1.5) - 1}).reduce(function(prev, curr) { return prev + curr});;
+integers = integers.sort(function (a, b) { return b - a}).filter(function(i) {return i < 19}).map(function(i) { return (i * 1.5) - 1}).reduce(function(prev, curr) { return prev + curr});;
 
 console.log(integers);
--- spa/seasonal-sales/seasonal-sales.js
+++ spa/seasonal-sales/seasonal-sales.js
@@ -1,8 +1,28 @@
-function createProductObj () {
-  return;
+//get reference to product output in element in DOM
+var productOutputEl = document.getElementById('productOutput');
+
+
+function displayProducts () {
+  var productsData = JSON.parse(myProductRequest.responseText);
+  console.log("productsData", productsData);
+
+  var products = productsData.products;
+
+  for (var i=0; i < products.length; i++){
+    var currentProduct = products[i];
+    console.log("currentProduct", currentProduct);
+    var productName = currentProduct.name;
+    var productPrice = currentProduct.price;
+    console.log("productName", productName);  
+    var productHTML = `<div class="card"><h4>Product: ${productName}</h4><h4>Price: $${productPrice}</h4></div>`;
+    productOutputEl.innerHTML += productHTML;
+  }
 }
 
-function createCategoryObj () {
+
+
+
+function displayCategories () {
   return;
 }
 
@@ -10,7 +30,7 @@ function createCategoryObj () {
 
 var myProductRequest = new XMLHttpRequest();
 
-myProductRequest.addEventListener("load", createProductObj);
+myProductRequest.addEventListener("load", displayProducts);
 myProductRequest.open("GET", "products.json");
 myProductRequest.send();
 console.log(myProductRequest);
@@ -18,6 +38,6 @@ console.log(myProductRequest);
 
 var myCategoryRequest = new XMLHttpRequest();
 
-myCategoryRequest.addEventListener("load", createCategoryObj);
+myCategoryRequest.addEventListener("load", displayCategories);
 myCategoryRequest.open("GET", "products.json");
 myCategoryRequest.send();
