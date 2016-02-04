12a8f52987e84dfb32bcee43264f6c1f8b203458
Thomas Buida <thomasbuida@gmail.com>
2016-02-04 17:12:49 -0600

    Carnivore and herbivore animals dynamically added to HTML page

--- spa/iife-xhr/iife-xhr.js
+++ spa/iife-xhr/iife-xhr.js
@@ -1,16 +1,19 @@
 function showCarnivores (carnivores) {
-  for (var animal in carnivores){
-    console.log(carnivores[animal]);
-  }
-
+  var carnivoreHTML= "";
+  var carnivoreEl = document.getElementById('carnivore');
+  carnivores.forEach(function (currentCarnivore){
+    carnivoreHTML += `<div>${currentCarnivore}</div>`;
+  })
+  carnivoreEl.innerHTML = carnivoreHTML;
 }
 
 function showHerbivores (herbivores) {
-  for (var animal in herbivores){
-    console.log(herbivores[animal]);
-  }
-
-
+  var herbivoreHTML= "";
+  var herbivoreEl = document.getElementById('herbivore');
+  herbivores.forEach(function (currentherbivore){
+    herbivoreHTML += `<div>${currentherbivore}</div>`;
+  })
+  herbivoreEl.innerHTML = herbivoreHTML;
 }
 
 Predator.loadCarnivores(showCarnivores);
