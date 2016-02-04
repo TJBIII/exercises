b34ee3c520327021e08fc340b54a93e4aa3eb77d
Thomas Buida <thomasbuida@gmail.com>
2016-02-04 15:09:38 -0600

    Added if statement to load functions to prevent multiple XHR calls when not needed

--- spa/iife-xhr/Predator.js
+++ spa/iife-xhr/Predator.js
@@ -4,32 +4,39 @@ var Predator = (function () {
 
   return {
     loadCarnivores: function (callbackToInvoke) {
-      var loader = new XMLHttpRequest();
-      loader.open("GET", "carnivores.json");
-      loader.send();
+      if (carnivores.length === 0){
+        var loader = new XMLHttpRequest();
+        loader.open("GET", "carnivores.json");
+        loader.send();
 
-      loader.addEventListener("load", function () {
-        // Set the value of the private array
-        carnivores = JSON.parse(this.responseText).carnivoresList;
+        loader.addEventListener("load", function () {
+          // Set the value of the private array
+          carnivores = JSON.parse(this.responseText).carnivoresList;
 
-        // Invoke the callback function so that the caller knows
-        // that the process is complete. Make sure to pass the 
-        // carnivore array as an argument.
+          // Invoke the callback function so that the caller knows
+          // that the process is complete. Make sure to pass the 
+          // carnivore array as an argument.
+          callbackToInvoke(carnivores);
+        });
+      } else {
         callbackToInvoke(carnivores);
-      });
+      }
     },
     loadHerbivores: function (callbackToInvoke) {
+      if (herbivores.length === 0){
+        var loader = new XMLHttpRequest();
+        loader.open("GET", "herbivores.json");
+        loader.send();
 
-      var loader = new XMLHttpRequest();
-      loader.open("GET", "herbivores.json");
-      loader.send();
+        loader.addEventListener("load", function(){
+          herbivores = JSON.parse(this.responseText).herbivoresList;
 
-      loader.addEventListener("load", function(){
-        herbivores = JSON.parse(this.responseText).herbivoresList;
+          callbackToInvoke(herbivores);
 
+        });
+      } else{
         callbackToInvoke(herbivores);
-
-    });
+      }
+    } 
   }
-}
 })();
