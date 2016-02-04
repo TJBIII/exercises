e311df68bba9d5ed1731df4d81b0cbfd270c5cef
Thomas Buida <thomasbuida@gmail.com>
2016-02-04 10:59:15 -0600

    Discount selector working

--- spa/seasonal-sales/seasonal-sales.js
+++ spa/seasonal-sales/seasonal-sales.js
@@ -1,6 +1,13 @@
 //get reference to product output in element in DOM
 var productOutputEl = document.getElementById('productOutput');
 
+var selectEl = document.getElementById('season-select');
+
+var productsData;
+var categoryData;
+
+var products;
+var categories;
 
 function displayProducts () {
 
@@ -10,14 +17,18 @@ function displayProducts () {
   myCategoryRequest.send();
 
 
-  var productsData = JSON.parse(myProductRequest.responseText);
-  var products = productsData.products;
+  productsData = JSON.parse(myProductRequest.responseText);
+  products = productsData.products;
 
   function displayAll() {
 
-    var categoryData = JSON.parse(myCategoryRequest.responseText);
-    var categories = categoryData.categories;
+    categoryData = JSON.parse(myCategoryRequest.responseText);
+    categories = categoryData.categories;
 
+    selectEl.addEventListener("change", update);
+
+    update();
+/*
     // console.log(products);
     // console.log(categories);
 
@@ -45,7 +56,55 @@ function displayProducts () {
       productHTML += `<h4>Dept: ${productDept}</h4></div>`;
       productOutputEl.innerHTML += productHTML;
     }
+
+    selectEl.addEventListener("change", function(){
+        console.log("changed!", selectEl.value);
+      })
+  } */
   }
+}
+
+function update() {
+  productOutputEl.innerHTML = "";
+
+  var selectedDiscountId = selectEl.value;
+  console.log("selectedDiscountId", selectedDiscountId);
+
+  for (var i=0; i < products.length; i++){
+    var currentProduct = products[i];
+    // console.log("currentProduct", currentProduct);
+
+    var productName = currentProduct.name;
+    // console.log("productName", productName);
+
+    var productPrice = currentProduct.price;
+
+
+    var productCatId = currentProduct.category_id;
+    //console.log("productCatId", productCatId);
+
+    for (var category in categories){
+      var currentCat = categories[category];
+      var catDiscount = currentCat.discount;
+
+      var catId = currentCat.id;
+
+      if (catId === productCatId){
+        var productDept = currentCat.name;
+      }
+
+      if (productCatId.toString() === selectedDiscountId){
+        //console.log("discount applied");
+        productPrice =(productPrice * (1-catDiscount)).toFixed(2);
+      }
+    }
+
+    var productHTML = `<div class="card"><h4>Product: ${productName}</h4>`;
+    productHTML += `<h4>Price: $${productPrice}</h4>`;
+    productHTML += `<h4>Dept: ${productDept}</h4></div>`;
+    productOutputEl.innerHTML += productHTML;
+  }
+
 }
 
 
