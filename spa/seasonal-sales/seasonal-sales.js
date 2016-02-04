de0aa7a28328b1e4c10b104bb8cccf0ee5f156bd
Thomas Buida <thomasbuida@gmail.com>
2016-02-04 10:25:35 -0600

    Dept names displaying on each card

--- spa/seasonal-sales/seasonal-sales.js
+++ spa/seasonal-sales/seasonal-sales.js
@@ -3,41 +3,54 @@ var productOutputEl = document.getElementById('productOutput');
 
 
 function displayProducts () {
-  var productsData = JSON.parse(myProductRequest.responseText);
-  console.log("productsData", productsData);
 
+  var myCategoryRequest = new XMLHttpRequest();
+  myCategoryRequest.addEventListener("load", displayAll);
+  myCategoryRequest.open("GET", "categories.json");
+  myCategoryRequest.send();
+
+
+  var productsData = JSON.parse(myProductRequest.responseText);
   var products = productsData.products;
 
-  for (var i=0; i < products.length; i++){
-    var currentProduct = products[i];
-    console.log("currentProduct", currentProduct);
-    var productName = currentProduct.name;
-    var productPrice = currentProduct.price;
-    console.log("productName", productName);  
-    var productHTML = `<div class="card"><h4>Product: ${productName}</h4><h4>Price: $${productPrice}</h4></div>`;
-    productOutputEl.innerHTML += productHTML;
+  function displayAll() {
+
+    var categoryData = JSON.parse(myCategoryRequest.responseText);
+    var categories = categoryData.categories;
+
+    // console.log(products);
+    // console.log(categories);
+
+    for (var i=0; i < products.length; i++){
+      var currentProduct = products[i];
+      // console.log("currentProduct", currentProduct);
+      var productName = currentProduct.name;
+      var productPrice = currentProduct.price;
+      // console.log("productName", productName);  
+
+      var productCatId = currentProduct.category_id;
+
+      for (var category in categories){
+        var currentCat = categories[category];
+
+        var catId = currentCat.id;
+
+        if (catId === productCatId){
+          var productDept = currentCat.name;
+        }
+      }
+
+      var productHTML = `<div class="card"><h4>Product: ${productName}</h4>`;
+      productHTML += `<h4>Price: $${productPrice}</h4>`;
+      productHTML += `<h4>Dept: ${productDept}</h4></div>`;
+      productOutputEl.innerHTML += productHTML;
+    }
   }
 }
 
 
 
-
-function displayCategories () {
-  return;
-}
-
-
-
 var myProductRequest = new XMLHttpRequest();
-
 myProductRequest.addEventListener("load", displayProducts);
 myProductRequest.open("GET", "products.json");
 myProductRequest.send();
-console.log(myProductRequest);
-
-
-var myCategoryRequest = new XMLHttpRequest();
-
-myCategoryRequest.addEventListener("load", displayCategories);
-myCategoryRequest.open("GET", "products.json");
-myCategoryRequest.send();
