f75d073658d997ad36af8a5c79e9a0c668f227a0
Thomas Buida <thomasbuida@gmail.com>
2016-02-05 15:30:23 -0600

    Person cards on page and styled correctly

--- spa/wysiwyg/famous.js
+++ spa/wysiwyg/famous.js
@@ -1,18 +1,9 @@
-var people = [{
-    title: "Samurai",
-    name: "Tomoe Gozen",
-    bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-    lifespan: {
-      birth: 1747,
-      death: 1797
-    }
-  },
+var people = [
   {
     title: "Artist",
     name: "Pablo Picasso",
     bio: "A Spanish painter, sculptor, printmaker, ceramicist, stage designer, poet and playwright.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Portrait_de_Picasso%2C_1908.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Portrait_de_Picasso%2C_1908.jpg",
     lifespan: {
       birth: 1881,
       death: 1973
@@ -22,9 +13,50 @@ var people = [{
     title: "Mathematician",
     name: "Gottfried Leibniz",
     bio: "A German mathematician and philosopher who is believed to have developed calculus independently of Isaac Newton.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Gottfried_Wilhelm_von_Leibniz.jpg"
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Gottfried_Wilhelm_von_Leibniz.jpg",
     lifespan: {
       birth: 1881,
       death: 1973
     }
-  }]
+  },
+  {
+    title: "Physicist",
+    name: "Richard Feynman",
+    bio: "An American theoretical physicist known for his work in QED, particle physics, and developing the atomic bomb.",
+    image: "https://upload.wikimedia.org/wikipedia/en/4/42/Richard_Feynman_Nobel.jpg",
+    lifespan: {
+      birth: 1918,
+      death: 1988
+    }
+  },
+  {
+    title: "Physicist",
+    name: "Enrico Fermi",
+    bio: "An Italian physicist, who created the world's first nuclear reactor, the Chicago Pile-1.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Enrico_Fermi_1943-49.jpg/500px-Enrico_Fermi_1943-49.jpg",
+    lifespan: {
+      birth: 1901,
+      death: 1954
+    }
+  },
+  {
+    title: "Mathematician",
+    name: "Carl Gauss",
+    bio: "A German mathematician who had an exceptional influence in many fields of mathematics and science and is ranked as one of history's most influential mathematicians.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Carl_Friedrich_Gauss.jpg",
+    lifespan: {
+      birth: 1777,
+      death: 1855
+    }
+  },
+  {
+    title: "Artist",
+    name: "Henri Cartier-Bresson",
+    bio: "A French photographer considered the master of candid photography, and an early user of 35 mm film.",
+    image: "https://upload.wikimedia.org/wikipedia/en/7/75/Henri_Cartier-Bresson.jpg",
+    lifespan: {
+      birth: 1908,
+      death: 2004
+    }
+  }
+];
---
+++ spa/wysiwyg/wysiwyg.js
@@ -0,0 +1,46 @@
+//get reference to container element in DOM
+var containerEl = document.getElementById('output');
+
+var title;
+var name;
+var bio;
+var imgLink;
+var lifespan;
+var personHTML;
+
+people.forEach(function(person) {
+  //start with empty string
+  personHTML = "";
+
+  title = person.title;
+  name = person.name;
+  bio = person.bio;
+  imgLink = person.image;
+  lifespan = person.lifespan;
+
+  personHTML += `<div class="person"><header>${name} - ${title}</header>`; 
+  personHTML += `<section><img width="150px" src="${imgLink}"><p>${bio}</p></section>`;
+  personHTML += `<footer>${lifespan.birth} - ${lifespan.death}</footer></div>`;
+
+  containerEl.innerHTML += personHTML;
+});
+
+var personsEls = document.getElementsByClassName('person');
+
+
+for (var i = 0; i < personsEls.length; i++){
+  personsEls[i].addEventListener("click", function(e){
+    focusedOn(e);
+  });
+}
+
+
+function focusedOn(e){
+  console.log(e.target);
+  e.target.toggleClass("bordered");
+
+
+}
+
+
+
