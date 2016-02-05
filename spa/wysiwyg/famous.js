e2e015ca9ec100bdfd3a0b2f4626ff9ab6afc037
Thomas Buida <thomasbuida@gmail.com>
2016-02-05 13:49:12 -0600

    Initial WYSIWYG setup

---
+++ spa/wysiwyg/famous.js
@@ -0,0 +1,30 @@
+var people = [{
+    title: "Samurai",
+    name: "Tomoe Gozen",
+    bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
+    lifespan: {
+      birth: 1747,
+      death: 1797
+    }
+  },
+  {
+    title: "Artist",
+    name: "Pablo Picasso",
+    bio: "A Spanish painter, sculptor, printmaker, ceramicist, stage designer, poet and playwright.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Portrait_de_Picasso%2C_1908.jpg"
+    lifespan: {
+      birth: 1881,
+      death: 1973
+    }
+  },
+  {
+    title: "Mathematician",
+    name: "Gottfried Leibniz",
+    bio: "A German mathematician and philosopher who is believed to have developed calculus independently of Isaac Newton.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Gottfried_Wilhelm_von_Leibniz.jpg"
+    lifespan: {
+      birth: 1881,
+      death: 1973
+    }
+  }]
